3ea4dd7e85b1fc078020381e4ab29f50302c541e
SwathantraPulicherla <swathantra.pulicherla@requisimus.com>
2026-01-22 11:06:20 +0530

    latest

--- tests/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/SwathantraPulicherla/workspaces/UnitTestGen/RailwaySignalSystem
-# Build directory: C:/Users/SwathantraPulicherla/workspaces/UnitTestGen/RailwaySignalSystem/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("src")
-subdirs("tests")
